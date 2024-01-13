00f7fe88f25573dac8ccdebb4f59bd4b07fe602a
shahid <wrteamshahid03@gmail.com>
2024-01-13 13:22:12 +0530

    Workchain hover fixed

--- src/Components/WorkChain.jsx
+++ src/Components/WorkChain.jsx
@@ -57,96 +57,103 @@ const WorkChain = () => {
     //     workNumb.style.transition = "top 0.3s ease"
 
     // }
-  return (
-    <section id="workChain" className='container'>
+    return (
+        <section id="workChain" className='container'>
 
-                <div className="workWrapper">
-                    <div className="row">
-                        <div className="col-sm-12 col-md-12 col-lg-12">
-                            <div className="workUpperDiv">
-                                <h4 className='common_span'> Our Work<span> Chain</span></h4>
-                                <span className='workHeadlines'>From Concept to Creation <span>Simplifying Your Project Journey</span></span>
-                            </div>
+            <div className="workWrapper">
+                <div className="row">
+                    <div className="col-sm-12 col-md-12 col-lg-12">
+                        <div className="workUpperDiv">
+                            <h4 className='common_span'> Our Work<span> Chain</span></h4>
+                            <span className='workHeadlines'>From Concept to Creation <span>Simplifying Your Project Journey</span></span>
                         </div>
-                        <div className="col-sm-12 col-md-12 col-lg-12">
-                            <div className="workBottDiv">
-                                <div className="workCardsWrapper" style={{
-                                    background: `url(${workLines.src})`,
-                                    backgroundSize: "contain",
-                                    backgroundPosition: "center",
-                                    backgroundRepeat: "no-repeat",
-                                }}>
+                    </div>
+                    <div className="col-sm-12 col-md-12 col-lg-12">
+                        <div className="workBottDiv">
+                            <div className="workCardsWrapper" style={{
+                                background: `url(${workLines.src})`,
+                                backgroundSize: "contain",
+                                backgroundPosition: "center",
+                                backgroundRepeat: "no-repeat",
+                            }}>
 
-                                    <div className="worklowerRow">
+                                <div className="worklowerRow">
 
-                                        <div className="row">
-                                            <div className="col-sm-12 col-md-6 col-lg-3">
-                                                <div className="workCard" id='wCard1' data-aos="fade-down"  data-aos-once="true" data-aos-duration="800">
+                                    <div className="row">
+
+                                        <div className="col-sm-12 col-md-6 col-lg-3">
+                                            <div className="workCard" id='wCard1' data-aos="fade-down" data-aos-once="true" data-aos-duration="800">
+                                                <div className="custom">
                                                     <span className='workNumb num1'>1</span>
-                                                    <Image height={0} width={0} loading="lazy"  src={workImg1} alt="" className='workImg' onMouseEnter={num1} onMouseLeave={num1Back} />
-
-                                                    <h5 className='workTitle'>Choose a service</h5>
-
-                                                    <span className='workDesc'>Choose the service from our list of solutions, Mobile Development, Web Development, UI/UX Design etc.</span>
-
+                                                    <Image height={0} width={0} loading="lazy" src={workImg1} alt="" className='workImg' />
                                                 </div>
+                                                <h5 className='workTitle'>Choose a service</h5>
+                                                <span className='workDesc'>Choose the service from our list of solutions, Mobile Development, Web Development, UI/UX Design etc.</span>
                                             </div>
+                                        </div>
 
-                                            <div className="col-sm-12 col-md-6 col-lg-3">
-                                                <div className="workCard workCardReverse" id='wCard2' data-aos="fade-up"  data-aos-once="true" data-aos-duration="800">
+                                        <div className="col-sm-12 col-md-6 col-lg-3">
+                                            <div className="workCard workCardReverse" id='wCard2' data-aos="fade-up" data-aos-once="true" data-aos-duration="800">
+                                                <div className="custom">
                                                     <span className='workNumb workRevNumb num2'>2</span>
                                                     <div className="workImgDiv">
 
-                                                        <Image height={0} width={0} loading="lazy"  src={workImg2} alt="" className='workImg' onMouseEnter={num2} onMouseLeave={num2Back} />
+                                                        <Image height={0} width={0} loading="lazy" src={workImg2} alt="" className='workImg' />
                                                     </div>
-
-
-                                                    <span className='workDesc'>Connect with our professional experts and have a detailed discussion about your requirement.</span>
-
-                                                    <h5 className='workTitle'>Request a meeting</h5>
-
                                                 </div>
-                                            </div>
 
-                                            <div className="col-sm-12 col-md-6 col-lg-3">
-                                                <div className="workCard" id='wCard3' data-aos="fade-down"  data-aos-once="true" data-aos-duration="800">
+
+
+                                                <span className='workDesc'>Connect with our professional experts and have a detailed discussion about your requirement.</span>
+
+                                                <h5 className='workTitle'>Request a meeting</h5>
+
+                                            </div>
+                                        </div>
+
+                                        <div className="col-sm-12 col-md-6 col-lg-3">
+                                            <div className="workCard" id='wCard3' data-aos="fade-down" data-aos-once="true" data-aos-duration="800">
+                                                <div className="custom">
                                                     <span className='workNumb num3'>3</span>
-                                                    <Image height={0} width={0} loading="lazy"  src={workImg3} alt="" className='workImg' onMouseEnter={num3} onMouseLeave={num3Back} />
-
-                                                    <h5 className='workTitle'>Start planning</h5>
-
-                                                    <span className='workDesc'>Detailed analysis and build a roadmap of what exactly you are looking for.</span>
-
+                                                    <Image height={0} width={0} loading="lazy" src={workImg3} alt="" className='workImg' />
                                                 </div>
+
+                                                <h5 className='workTitle'>Start planning</h5>
+
+                                                <span className='workDesc'>Detailed analysis and build a roadmap of what exactly you are looking for.</span>
+
                                             </div>
+                                        </div>
 
-                                            <div className="col-sm-12 col-md-6 col-lg-3">
-                                                <div className="workCard workCardReverse" id='wCard4' data-aos="fade-up"  data-aos-once="true" data-aos-duration="800">
+                                        <div className="col-sm-12 col-md-6 col-lg-3">
+                                            <div className="workCard workCardReverse" id='wCard4' data-aos="fade-up" data-aos-once="true" data-aos-duration="800">
+                                                <div className="custom">
                                                     <span className='workNumb workRevNumb num4'>4</span>
-                                                    <Image height={0} width={0} loading="lazy"  src={workImg4} alt="" className='workImg' onMouseEnter={num4} onMouseLeave={num4Back} />
-
-
-                                                    <span className='workDesc'>Time to build real solutions that work for you.</span>
-
-                                                    <h5 className='workTitle'>Let's make it happen</h5>
-
+                                                    <Image height={0} width={0} loading="lazy" src={workImg4} alt="" className='workImg' />
                                                 </div>
+
+
+                                                <span className='workDesc'>Time to build real solutions that work for you.</span>
+
+                                                <h5 className='workTitle'>Let's make it happen</h5>
+
                                             </div>
                                         </div>
                                     </div>
-
                                 </div>
 
                             </div>
+
                         </div>
                     </div>
-
-
-
-
                 </div>
-            </section >
-  )
+
+
+
+
+            </div>
+        </section >
+    )
 }
 
 export default WorkChain
