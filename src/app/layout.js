8f6e6f128d2a6b3a89c8906efe124f9358f86731
shahid <wrteamshahid03@gmail.com>
2024-04-25 13:04:20 +0530

    added id in gtm script

--- src/app/layout.js
+++ src/app/layout.js
@@ -70,7 +70,7 @@ export default function RootLayout({ children }) {
 
 
         {/* <!-- Google Tag Manager --> */}
-        <Script dangerouslySetInnerHTML={{
+        <Script id="gtmScript" dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
