5c882965c4a8a06c8ed04242966389810630679b
shahid <wrteamshahid03@gmail.com>
2024-04-25 13:01:36 +0530

    move gtm out of Head

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,19 +42,6 @@ export default function RootLayout({ children }) {
 
               <link rel="shortcut icon" href="/favicon.ico" sizes="32x32" type="image/png" />
 
-
-
-              {/* <!-- Google Tag Manager --> */}
-              <Script dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-MG5P53R')
-
-                `}}></Script>
-              {/* <!-- End Google Tag Manager --> */}
-
             </Head>
 
 
@@ -80,6 +67,18 @@ export default function RootLayout({ children }) {
             </>
           </Providers>
         </QueryClientProvider>
+
+
+        {/* <!-- Google Tag Manager --> */}
+        <Script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-MG5P53R')
+
+                `}}></Script>
+        {/* <!-- End Google Tag Manager --> */}
 
         {/* <!-- Google Tag Manager (noscript) --> */}
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MG5P53R"
