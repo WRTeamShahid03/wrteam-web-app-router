72a2c96805ae7f09efa287b368380fccb9f77217
shahid <wrteamshahid03@gmail.com>
2024-04-25 12:53:48 +0530

    gtm added

--- src/app/layout.js
+++ src/app/layout.js
@@ -45,14 +45,14 @@ export default function RootLayout({ children }) {
 
 
               {/* <!-- Google Tag Manager --> */}
-              <script dangerouslySetInnerHTML={{
+              <Script dangerouslySetInnerHTML={{
                 __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                                         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                     })(window,document,'script','dataLayer','GTM-MG5P53R')
 
-                `}}></script>
+                `}}></Script>
               {/* <!-- End Google Tag Manager --> */}
 
             </Head>
