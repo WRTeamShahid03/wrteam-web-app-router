d463dcb7dc7133accca94dabefc2181231c6246b
Sumit-Mayani <sumi.mayani@gmail.com>
2024-09-03 10:07:07 +0530

    app router details page issue fix done in seo

--- src/app/product-detail-page/[slug]/page.jsx
+++ src/app/product-detail-page/[slug]/page.jsx
@@ -1,50 +1,37 @@
-import { GET_PRODUCTS, GET_SEO_SETTINGS } from '@/utils/api';
-import axios from 'axios';
-import dynamic from 'next/dynamic'
-// const ProductDetailsPage = dynamic(() => import('@/Components/pagesComponent/ProductDetailsPage'), { ssr: false })
+import { GET_PRODUCTS } from "@/utils/api";
+import axios from "axios";
+import dynamic from "next/dynamic";
 const ProductDeatailPage = dynamic(() => import('@/Components/pagesComponent/ProductDeatailPage'), { ssr: false })
+
+const getDetailsProductData = async (params) => {
+    const slug = params?.slug;
+    try {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}${process.env.NEXT_PUBLIC_END_POINT}${GET_PRODUCTS}?slug=${slug}`);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+export async function generateMetadata({ params }) {
+    const getDatafromProductApi = await getDetailsProductData(params);
+    return {
+        title: getDatafromProductApi.data?.seo_title,
+        description: getDatafromProductApi && getDatafromProductApi?.data?.seo_keywords,
+        keywords: getDatafromProductApi && getDatafromProductApi?.data?.seo_description,
+        openGraph: {
+            images: getDatafromProductApi?.data?.ogImage ? [getDatafromProductApi?.data?.ogImage] : [],
+        },
+    };
+}
+
 const Index = async ({ params }) => {
-    // const generateDataforDetails = async () => {
-    //     // console.log('slug === > ',slug.params)
-    //     const slug = params?.slug;
-    //     try {
-    //         const response = await axios.get(
-    //             `${process.env.NEXT_PUBLIC_API_URL}${process.env.NEXT_PUBLIC_END_POINT}${GET_PRODUCTS}?slug=${slug}`
-    //         );
-    //         const SEOData = response.data;
-    //         async function generateMetadata() {
-    //             return {
-    //                 title: SEOData && SEOData?.data?.seo_title,
-    //                 description: SEOData && SEOData?.data?.seo_keywords,
-    //                 keywords: SEOData && SEOData?.data?.seo_description,
-    //                 openGraph: {
-    //                     images: SEOData?.data?.ogImage ? [SEOData?.data?.ogImage] : [],
-    //                 },
-    //             }
-    //         }
-    //         generateMetadata()
-    //         // console.log('SEOData =====>  ',SEOData)
-    //         return {
-    //             // title: SEOData && SEOData?.data?.seo_title,
-    //             // description: SEOData && SEOData?.data?.seo_keywords,
-    //             // keywords: SEOData && SEOData?.data?.seo_description,
-    //             // openGraph: {
-    //             //     images: SEOData?.data?.ogImage ? [SEOData?.data?.ogImage] : [],
-    //             // },
-    //             SEOData
-    //         };
-    //     } catch (error) {
-    //         console.error("Error fetching MetaData:", error);
-    //         return null;
-    //     }
-    // };
-    const SEOData = await generateDataforDetails();
-    // console.log('SEOData =>', SEOData)
+    const getDatafromProductApi = await getDetailsProductData(params);
     return (
         <>
-            {console.log('SEOData =>', SEOData)}
-            <ProductDeatailPage detailPageData={SEOData} />
+            <ProductDeatailPage detailPageData={getDatafromProductApi} />
         </>
-    )
-}
-export default Index
+    );
+};
+export default Index;
